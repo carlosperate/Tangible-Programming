1198fde4e8bfe921ee7a8bbf43f6677e1a005516
Carlos Pereira Atencio <carlospa87@gmail.com>
2014-05-31 16:00:57 +0100

    Added TopCodes project from Elliot.

---
+++ prototypes/TopCodes/main.java
@@ -0,0 +1,30 @@
+
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+public class main {
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		topCodesController topCodesController = new topCodesController();
+
+		String x = topCodesController.codeNumbers();
+
+
+		System.out.println(x);
+		
+	}
+
+}
